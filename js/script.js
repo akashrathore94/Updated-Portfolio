fb8d75a0b22b122a81e1e0cb10950ad48c8774b4
akashrathore94 <akashrathour2011@gmail.com>
2020-08-17 17:54:35 +0530

    script. js updated

--- js/script.js
+++ js/script.js
@@ -1,46 +1,40 @@
-const sections = document.querySelectorAll('section');
-const links = document.querySelectorAll('.link');
+const links = document.querySelectorAll(".link");
 
-const options = {
-    threshold: 0.2
-};
+function removeActive() {
+  links.forEach((link) => {
+    link.classList.remove("active");
+  });
+}
 
-let observer = new IntersectionObserver(navCheck, options);
-
-function navCheck(entries) {
-    entries.forEach(entry => {        
-        if(!entry.isIntersecting){
-            return;
-        } else {
-            const className = entry.target.className;
-            if(className == 'container demo home'){
-                links.forEach(link => {
-                       link.classList.remove('active');
-                });
-                document.querySelector('.except').classList.add('active');
-                return;
-            }
-            links.forEach(link => {
-
-               if(link.getAttribute('data-page') == className){
-                   links.forEach(link => {
-                       link.classList.remove('active');
-                   });
-                   link.classList.add('active');
-               }  
-            });
-        }
-    });
-};
-
-sections.forEach(section => {
-    observer.observe(section);
+var waypoint1 = new Waypoint({
+  element: document.getElementById("about"),
+  handler: function (direction) {
+    removeActive();
+    if (direction == "down")
+      document.querySelector(".about-link").classList.add("active");
+    else document.querySelector(".home-link").classList.add("active");
+  },
+  offset: "55%",
 });
 
+var waypoint2 = new Waypoint({
+  element: document.getElementById("portfolio"),
+  handler: function (direction) {
+    removeActive();
+    if (direction == "down")
+      document.querySelector(".portfolio-link").classList.add("active");
+    else document.querySelector(".about-link").classList.add("active");
+  },
+  offset: "55%",
+});
 
-function myFunction1(){
-    document.querySelector('.backstage').style.display = "block";
-}
-function myclose1(){
-    document.querySelector('.backstage').style.display = "none";
-}
+var waypoint3 = new Waypoint({
+  element: document.getElementById("contact"),
+  handler: function (direction) {
+    removeActive();
+    if (direction == "down")
+      document.querySelector(".contact-link").classList.add("active");
+    else document.querySelector(".portfolio-link").classList.add("active");
+  },
+  offset: "55%",
+});
