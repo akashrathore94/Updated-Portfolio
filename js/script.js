bd8b6af9ffb32206aea7066498496e1f64cc31ce
akashrathore94 <akashrathour2011@gmail.com>
2020-08-20 02:17:01 +0530

    js updated

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ function removeActive() {
   });
 }
 
-var waypoint1 = new Waypoint({
+var aboutSec = new Waypoint({
   element: document.getElementById("about"),
   handler: function (direction) {
     removeActive();
@@ -28,7 +28,7 @@ var waypoint1 = new Waypoint({
   offset: "55%",
 });
 
-var waypoint2 = new Waypoint({
+var portfolioSec = new Waypoint({
   element: document.getElementById("portfolio"),
   handler: function (direction) {
     removeActive();
@@ -46,7 +46,7 @@ var waypoint2 = new Waypoint({
   offset: "55%",
 });
 
-var waypoint3 = new Waypoint({
+var contactSec = new Waypoint({
   element: document.getElementById("contact"),
   handler: function (direction) {
     removeActive();
@@ -66,38 +66,126 @@ var waypoint3 = new Waypoint({
 
 /**********ANIMATIONS *************/
 
-var waypoint4 = new Waypoint({
-  element: document.querySelector(".bullet"),
-  handler: function () {
-    document
-      .querySelectorAll(".bullet")
-      .forEach((el) =>
-        el.classList.add("animate__animated", "animate__flipInX")
-      );
-  },
-  offset: "70%",
-});
-
 document
   .querySelectorAll(".del")
   .forEach((el) => el.style.setProperty("--animate-duration", "1.5s"));
 
-var waypoint5 = new Waypoint({
+var bul1 = new Waypoint({
+  element: document.querySelector(".js-bul1"),
+  handler: function () {
+    document
+      .querySelector(".js-bul1")
+      .classList.add("animate__animated", "animate__flipInX");
+  },
+  offset: "75%",
+});
+
+var bul2 = new Waypoint({
+  element: document.querySelector(".js-bul2"),
+  handler: function () {
+    document
+      .querySelector(".js-bul2")
+      .classList.add("animate__animated", "animate__flipInX");
+  },
+  offset: "75%",
+});
+
+var bul3 = new Waypoint({
+  element: document.querySelector(".js-bul3"),
+  handler: function () {
+    document
+      .querySelector(".js-bul3")
+      .classList.add("animate__animated", "animate__flipInX");
+  },
+  offset: "75%",
+});
+
+var bul4 = new Waypoint({
+  element: document.querySelector(".js-bul4"),
+  handler: function () {
+    document
+      .querySelector(".js-bul4")
+      .classList.add("animate__animated", "animate__flipInX");
+  },
+  offset: "75%",
+});
+
+var about = new Waypoint({
+  element: document.querySelector(".js-about"),
+  handler: function () {
+    document
+      .querySelector(".js-about")
+      .classList.add("animate__animated", "animate__fadeIn");
+  },
+  offset: "70%",
+});
+
+var skills = new Waypoint({
+  element: document.querySelector(".js-skills"),
+  handler: function () {
+    document
+      .querySelector(".js-skills")
+      .classList.add("animate__animated", "animate__fadeIn");
+  },
+  offset: "70%",
+});
+
+var backstageImg = new Waypoint({
   element: document.querySelector(".js-back-img"),
   handler: function () {
     document
       .querySelector(".js-back-img")
-      .classList.add("animate__animated", "animate__fadeInDown");
+      .classList.add("animate__animated", "animate__fadeInLeft");
   },
-  offset: "60%",
+  offset: "70%",
 });
 
-var waypoint6 = new Waypoint({
+var backstageTxt = new Waypoint({
   element: document.querySelector(".js-back-txt"),
   handler: function () {
     document
       .querySelector(".js-back-txt")
-      .classList.add("animate__animated", "animate__fadeInUp");
+      .classList.add("animate__animated", "animate__fadeInRight");
   },
-  offset: "60%",
+  offset: "70%",
+});
+
+var budgetyImg = new Waypoint({
+  element: document.querySelector(".js-budg-img"),
+  handler: function () {
+    document
+      .querySelector(".js-budg-img")
+      .classList.add("animate__animated", "animate__fadeInLeft");
+  },
+  offset: "70%",
+});
+
+var budgetyTxt = new Waypoint({
+  element: document.querySelector(".js-budg-txt"),
+  handler: function () {
+    document
+      .querySelector(".js-budg-txt")
+      .classList.add("animate__animated", "animate__fadeInRight");
+  },
+  offset: "70%",
+});
+
+var thirdImg = new Waypoint({
+  element: document.querySelector(".js-third-img"),
+  handler: function () {
+    document
+      .querySelector(".js-third-img")
+      .classList.add("animate__animated", "animate__fadeInLeft");
+  },
+  offset: "70%",
+});
+
+var thirdTxt = new Waypoint({
+  element: document.querySelector(".js-third-txt"),
+  handler: function () {
+    document
+      .querySelector(".js-third-txt")
+      .classList.add("animate__animated", "animate__fadeInRight");
+  },
+  offset: "70%",
 });
